4fc2c4cb72456b49c6ca17f72db835c58e1afa28
f1carreras <f1carreras1985@gmail.com>
2024-12-24 13:19:55 -0500

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer__wrapper">
         <section class="footer__section">
             <h2 class="footer__section-title">
-                <img src="{{ url('/favicon.ico') }}" style="height: 30px; vertical-align: sub" />
+                <img src="{{ url('/formula1_favicon.ico') }}" style="height: 30px; vertical-align: sub" />
                 <span class="top-nav__site-logo">{{ \config('other.title') }}</span>
             </h2>
             <p>{{ config('other.meta_description') }}</p>
