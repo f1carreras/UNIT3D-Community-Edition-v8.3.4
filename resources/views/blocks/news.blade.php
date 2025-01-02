05d51ea4d82d7c95e259cbcc3bc6568ca773b6d4
f1carreras <f1carreras1985@gmail.com>
2025-01-02 14:32:28 -0500

    Update news.blade.php

--- resources/views/blocks/news.blade.php
+++ resources/views/blocks/news.blade.php
@@ -41,7 +41,7 @@
                     </time>
                     <img
                         class="article-preview__image"
-                        src="{{ url($article->image ? 'files/img/' . $article->image : 'img/missing-image.png') }}"
+                        src="{{ url($article->image ? 'files/img/' . $article->image : 'img/missing-image2.png') }}"
                         alt=""
                     />
                 </header>
