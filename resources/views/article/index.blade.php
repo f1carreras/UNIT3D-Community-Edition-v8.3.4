06896fa2a0b86c262be3d163dad7938f00f2e0b2
f1carreras <f1carreras1985@gmail.com>
2025-01-02 14:28:39 -0500

    Update index.blade.php

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -37,7 +37,7 @@
                 </time>
                 <img
                     class="article-preview__image"
-                    src="{{ url($article->image ? 'files/img/' . $article->image : 'img/missing-image.png') }}"
+                    src="{{ url($article->image ? 'files/img/' . $article->image : 'img/missing-image2.png') }}"
                     alt=""
                 />
             </header>
